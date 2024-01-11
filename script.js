6510a289744b82e55a0a0c19374d502f12de398b
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-01-11 12:29:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ const blockIdInput = document.getElementById("block_id");
 const colorIdInput = document.getElementById("color_id");
 const gridItems = document.querySelectorAll(".grid-item");
 const changeColor = document.getElementById("change_button");
-const reset = document.getElementById("Reset");
-changeColor.addEventListener("click", ()=>{
-	const blockId = blockIdInput.value;
-	const colorId = colorIdInput.value;
-	gridItems.forEach(item =>{
-		if(item.id === `grid-item-${blockId}`){
-			item.style.backgroundColor = colorId;
-			item.style.color = "white";
-		}
-	});
-});
-reset.addEventListener("click", () => {
+const reset = document.getElementById("reset_button");
+
+changeColor.addEventListener("click", () => {
+    const blockId = blockIdInput.value;
+    const colorId = colorIdInput.value;
+    
     gridItems.forEach(item => {
-        item.style.backgroundColor = "transparent";
-		item.style.color = "black";
+        if (item.id === `grid-item-${blockId}`) {
+            item.style.backgroundColor = colorId;
+            item.style.color = "white";
+        }
     });
 });
 
+reset.addEventListener("click", () => {
+    gridItems.forEach(item => {
+        item.style.backgroundColor = "transparent";
+        item.style.color = "black";
+    });
+});
